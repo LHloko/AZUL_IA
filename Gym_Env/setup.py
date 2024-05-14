49a5c5f50b347f9d32b82725b3cf1d1b078f8cdc
L. Balieiro <89412529+LHloko@users.noreply.github.com>
2024-05-14 13:54:09 -0300

    Add files via upload

---
+++ Gym_Env/setup.py
@@ -0,0 +1,13 @@
+"""
+Created on Tue May 14 13:31:08 2024
+
+@author: lbalieiro@lince.lab
+"""
+
+from setuptools import setup
+
+setup(
+    name="Azul_Env",
+    version="0.0.1",
+    install_requires=["gymnasium==0.28.0"],
+)
